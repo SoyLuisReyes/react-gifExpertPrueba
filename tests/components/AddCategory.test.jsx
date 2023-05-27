ad2c4b45a4335a9e79f91af34b37dc9a661d3972
luisreyes <tecwicho@gmail.com>
2023-05-26 22:41:38 -0600

    test

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -1,5 +1,16 @@
+import { fireEvent, render,screen } from '@testing-library/react';
+import { AddCategory } from '../../src/components/AddCategory';
+
 describe('pruebas addCategory', () => { 
     test('debe de cambiar el valor de la caja de texto', () => { 
+
+        render( <AddCategory onNewCategory={ ()  => {} } />);
+        const input = screen.getByRole('textbox');
+
+        fireEvent.input( input, { target: {value:'saitama'} });
+
+        expect( input.value).toBe('saitama');
+
         
     }) 
 })
