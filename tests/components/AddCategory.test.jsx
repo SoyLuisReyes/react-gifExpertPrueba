6deee4664cb5aa60bba1d938a3c3c210ab826c80
luisreyes <tecwicho@gmail.com>
2023-05-26 23:00:09 -0600

    jest

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -4,13 +4,26 @@ import { AddCategory } from '../../src/components/AddCategory';
 describe('pruebas addCategory', () => { 
     test('debe de cambiar el valor de la caja de texto', () => { 
 
+        //sujeto de prueba 
         render( <AddCategory onNewCategory={ ()  => {} } />);
         const input = screen.getByRole('textbox');
 
         fireEvent.input( input, { target: {value:'saitama'} });
 
         expect( input.value).toBe('saitama');
+    });
 
-        
-    }) 
+    test('Debe de llamar OnNewCategory si el input tiene un valor.', () => { 
+        const inputValue = 'Vader'
+
+        render( <AddCategory onNewCategory={ ()  => {} } />);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: {value:'saitama'} });
+        fireEvent.submit( form );
+
+        expect( input.value ).toBe('');
+
+    });
 })
